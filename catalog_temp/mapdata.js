b80161c1baad50a98d825afeaed4daa0e7344b35
AlexKnyshov <alexknyshov@gmail.com>
2020-11-07 15:15:04 -0800

    mapdata added

---
+++ catalog_temp/mapdata.js
@@ -0,0 +1,236 @@
+var testdata = {areas : {
+	'GT': {
+		value: 16,
+		tooltip: {
+			content: 'country: GT<br>16 spp.'
+		},
+	},
+	'GD': {
+		value: 8,
+		tooltip: {
+			content: 'country: GD<br>8 spp.'
+		},
+	},
+	'CR': {
+		value: 10,
+		tooltip: {
+			content: 'country: CR<br>10 spp.'
+		},
+	},
+	'FR': {
+		value: 4,
+		tooltip: {
+			content: 'country: FR<br>4 spp.'
+		},
+	},
+	'HU': {
+		value: 6,
+		tooltip: {
+			content: 'country: HU<br>6 spp.'
+		},
+	},
+	'MN': {
+		value: 7,
+		tooltip: {
+			content: 'country: MN<br>7 spp.'
+		},
+	},
+	'PL': {
+		value: 5,
+		tooltip: {
+			content: 'country: PL<br>5 spp.'
+		},
+	},
+	'ES': {
+		value: 2,
+		tooltip: {
+			content: 'country: ES<br>2 spp.'
+		},
+	},
+	'PR': {
+		value: 3,
+		tooltip: {
+			content: 'country: PR<br>3 spp.'
+		},
+	},
+	'MG': {
+		value: 2,
+		tooltip: {
+			content: 'country: MG<br>2 spp.'
+		},
+	},
+	'GH': {
+		value: 1,
+		tooltip: {
+			content: 'country: GH<br>1 spp.'
+		},
+	},
+	'PH': {
+		value: 1,
+		tooltip: {
+			content: 'country: PH<br>1 spp.'
+		},
+	},
+	'AU': {
+		value: 1,
+		tooltip: {
+			content: 'country: AU<br>1 spp.'
+		},
+	},
+	'DE': {
+		value: 4,
+		tooltip: {
+			content: 'country: DE<br>4 spp.'
+		},
+	},
+	'FI': {
+		value: 1,
+		tooltip: {
+			content: 'country: FI<br>1 spp.'
+		},
+	},
+	'AR': {
+		value: 1,
+		tooltip: {
+			content: 'country: AR<br>1 spp.'
+		},
+	},
+	'SD': {
+		value: 2,
+		tooltip: {
+			content: 'country: SD<br>2 spp.'
+		},
+	},
+	'YE': {
+		value: 1,
+		tooltip: {
+			content: 'country: YE<br>1 spp.'
+		},
+	},
+	'JP': {
+		value: 3,
+		tooltip: {
+			content: 'country: JP<br>3 spp.'
+		},
+	},
+	'AT': {
+		value: 1,
+		tooltip: {
+			content: 'country: AT<br>1 spp.'
+		},
+	},
+	'BE': {
+		value: 1,
+		tooltip: {
+			content: 'country: BE<br>1 spp.'
+		},
+	},
+	'CH': {
+		value: 1,
+		tooltip: {
+			content: 'country: CH<br>1 spp.'
+		},
+	},
+	'BG': {
+		value: 1,
+		tooltip: {
+			content: 'country: BG<br>1 spp.'
+		},
+	},
+	'IT': {
+		value: 1,
+		tooltip: {
+			content: 'country: IT<br>1 spp.'
+		},
+	},
+	'TR': {
+		value: 2,
+		tooltip: {
+			content: 'country: TR<br>2 spp.'
+		},
+	},
+	'MA': {
+		value: 1,
+		tooltip: {
+			content: 'country: MA<br>1 spp.'
+		},
+	},
+	'CN': {
+		value: 1,
+		tooltip: {
+			content: 'country: CN<br>1 spp.'
+		},
+	},
+	'GR': {
+		value: 1,
+		tooltip: {
+			content: 'country: GR<br>1 spp.'
+		},
+	},
+	'IQ': {
+		value: 1,
+		tooltip: {
+			content: 'country: IQ<br>1 spp.'
+		},
+	},
+	'NL': {
+		value: 1,
+		tooltip: {
+			content: 'country: NL<br>1 spp.'
+		},
+	},
+	'MX': {
+		value: 4,
+		tooltip: {
+			content: 'country: MX<br>4 spp.'
+		},
+	},
+	'BR': {
+		value: 6,
+		tooltip: {
+			content: 'country: BR<br>6 spp.'
+		},
+	},
+	'CU': {
+		value: 4,
+		tooltip: {
+			content: 'country: CU<br>4 spp.'
+		},
+	},
+	'GP': {
+		value: 1,
+		tooltip: {
+			content: 'country: GP<br>1 spp.'
+		},
+	},
+	'PA': {
+		value: 11,
+		tooltip: {
+			content: 'country: PA<br>11 spp.'
+		},
+	},
+	'CO': {
+		value: 3,
+		tooltip: {
+			content: 'country: CO<br>3 spp.'
+		},
+	},
+	'EC': {
+		value: 1,
+		tooltip: {
+			content: 'country: EC<br>1 spp.'
+		},
+	},
+	'HN': {
+		value: 1,
+		tooltip: {
+			content: 'country: HN<br>1 spp.'
+		},
+	},
+	'PY': {
+		value: 1,
+		tooltip: {
+			content: 'country: PY<br>1 spp.'
+		},
+	},
+}}
